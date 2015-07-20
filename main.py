b8b9e01995f39ae8ea6014086234342946237996
astha-agarwal <astha.juit@gmail.com>
2015-07-20 14:23:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+string =  input('Enter a string: ')
+length = len(string)
+if len == 0:
+  print 'Empty string'
+  return
+else:
+  reverse_string = ''
+  while length > 0:
+    reverse_string += string[length]
+    length = length - 1
+  return reverse_string
+
